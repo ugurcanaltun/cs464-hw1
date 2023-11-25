8a292383f6ccbe2deca2252838abff9a5ce44309
Uğur Can Altun <82045097+ugurcanaltun@users.noreply.github.com>
2023-11-25 11:45:08 +0300

    edited bernoulli model

--- q3main.py
+++ q3main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
+import time
 
 # Specifying a relative path to the local directory
 dirname = os.path.dirname(__file__)
@@ -12,6 +13,7 @@ y_train = pd.read_csv(filepath + '\\y_train.csv', header=None).values.ravel()
 x_test = pd.read_csv(filepath + '\\X_test.csv', delimiter=' ').values
 y_test = pd.read_csv(filepath + '\\y_test.csv', header=None).values.ravel()
 
+start_time = time.time()
 
 # Function to calculate multinomial prior and likelihood probabilities with no smoothing performed
 def calculate_probabilities_multinomial_nosmoothing(x_features_train, y_labels_train):
@@ -43,47 +45,6 @@ def calculate_probabilities_multinomial_nosmoothing(x_features_train, y_labels_t
     likelihood_prob_calculated = (feature_counts) / (np.sum(feature_counts, axis=1)[:, np.newaxis])
 
     return prior_prob_calculated, likelihood_prob_calculated
-
-# Function to predict via multinomial
-def predict_multinomial_bayes(x_features_test, prior_probabilites, likelihood_probabilities):
-    """In this function, we are making prediction according to multinomial naive bayes
-    model. We are performing the following formula:
-            yi = argmax( logP(Y=yk) + sum twj,i * logP(Xj/Y=yk), j=i to |V|)
-                   y
-    Args:
-        x_features_test (numpy.2darray): the number of the features in the test set
-        that we are going to use for prediction
-        prior_probabilites (numpy.1darray): the prior probabilites that we previously calculated
-        likelihood_probabilities (numpy.2darray): the likelihood probabilities that we 
-        previously calculated 
-
-    Returns:
-        argmax(numpy.1darray): we create an array from the indices of the greatest values
-        over axis 1, which indicates which class each instance in the test set according to
-        our prediction
-    """
-    # Taking logs of the probabilities, taking probabilities that are near zero as -10**12 instead of
-    # taking thier logs to avoid underflow
-    log_likelihood_prob = np.where(likelihood_probabilities < 0.0000000001, -10**12, np.log(likelihood_probabilities ,where = likelihood_probabilities > 0.0000000001))
-    #Doing matrix multiplication and making arrays suitable for matrix multiplication
-    log_probs = np.log(prior_probabilites) + np.dot(x_features_test, log_likelihood_prob.T)
-    return np.argmax(log_probs, axis=1)
-
-# Train the Multinomial Naive Bayes with no smoothing
-prior_prob, likelihood_prob = calculate_probabilities_multinomial_nosmoothing(x_train, y_train)
-y_pred = predict_multinomial_bayes(x_test, prior_prob, likelihood_prob)
-
-# Calculate the accuracy of multinomial bayes with no smoothing
-accuracy = np.mean(y_pred == y_test)
-print(f'Accuracy: {accuracy:.3f}')
-
-# Create the Confusion Matrix for multinomial bayes with no smoothing
-conf_matrix = np.zeros((len(np.unique(y_test)), len(np.unique(y_test))))
-for index, label in enumerate(y_test):
-    conf_matrix[y_pred[index], y_test[index]] += 1
-
-print('Confusion Matrix:')
-print(conf_matrix)
 
 # Function to calculate multinomial prior and likelihood probabilities with smoothing performed
 def calculate_probabilities_multinomial_smoothing(x_features_train, y_labels_train, alpha = 1):
@@ -118,22 +79,6 @@ def calculate_probabilities_multinomial_smoothing(x_features_train, y_labels_tra
 
     return prior_prob_calculated, likelihood_prob_calculated
 
-# Train the Multinomial Naive Bayes model with smoothing
-prior_prob, likelihood_prob = calculate_probabilities_multinomial_smoothing(x_train, y_train)
-y_pred = predict_multinomial_bayes(x_test, prior_prob, likelihood_prob)
-
-# Calculate accuracy
-accuracy = np.mean(y_pred == y_test)
-print(f'Accuracy: {accuracy:.3f}')
-
-# Create the Confusion Matrix for multinomial bayes with no smoothing
-conf_matrix = np.zeros((len(np.unique(y_test)), len(np.unique(y_test))))
-for index, label in enumerate(y_test):
-    conf_matrix[y_pred[index], y_test[index]] += 1
-
-print('Confusion Matrix:')
-print(conf_matrix)
-
 # Function to calculate bernoulli prior and likelihood probabilities with smoothing performed
 def calculate_probabilities_bernoulli(x_features_train, y_labels_train, alpha = 1):
     """Prior probabilities for each class label and likelihood probabilities of
@@ -141,8 +86,8 @@ def calculate_probabilities_bernoulli(x_features_train, y_labels_train, alpha = 
     model
     
     Args:
-        x_features_train (numpy.2darray): _description_
-        y_labels_train (numpy.1darray): _description_
+        x_features_train (numpy.2darray): number of features in each instance of the training data 
+        y_labels_train (numpy.1darray): labels of each instance of the training data
         alpha (int): Dirichlet prior constant. Defaults to 1.
 
     Returns:
@@ -167,25 +112,107 @@ def calculate_probabilities_bernoulli(x_features_train, y_labels_train, alpha = 
 
     return prior_probab_calculated, likelihood_prob_calculated
 
-# Function to predict
-def predict_bernoulli(x_features_test, prior_probabilities, likelihood_probabilites):
-    x_binary = (x_features_test > 0).astype(int)
-    log_likelihood_prob = np.where(likelihood_prob < 0.0000000001, -10**12, np.log(likelihood_prob,where = likelihood_prob > 0.0000000001))
-    log_probs = np.log(prior_prob) + np.dot(x_binary, log_likelihood_prob.T) + np.dot(1 - x_binary, np.log(1 - likelihood_prob).T)
+# Function to predict via multinomial naive bayes model
+def predict_multinomial_bayes(x_features_test, prior_probabilites, likelihood_probabilities):
+    """In this function, we are making prediction according to multinomial naive bayes
+    model. We are performing the following formula:
+            yi = argmax( logP(Y=yk) + sum twj,i * logP(Xj/Y=yk), j=i to |V|)
+                   y
+    Args:
+        x_features_test (numpy.2darray): the number of the features in the test set
+        that we are going to use for prediction
+        prior_probabilites (numpy.1darray): the prior probabilites that we previously calculated
+        likelihood_probabilities (numpy.2darray): the likelihood probabilities that we 
+        previously calculated 
+
+    Returns:
+        argmax(numpy.1darray): we create an array from the indices of the greatest values
+        over axis 1, which indicates which class each instance in the test set according to
+        our prediction
+    """
+    # Taking logs of the probabilities, taking probabilities that are near zero as -10**12 instead of
+    # taking thier logs to avoid underflow
+    log_likelihood_prob = np.where(likelihood_probabilities < 0.0000000001, -10**12, np.log(likelihood_probabilities ,where = likelihood_probabilities > 0.0000000001))
+    #Doing matrix multiplication and making arrays suitable for matrix multiplication
+    log_probs = np.log(prior_probabilites) + np.dot(x_features_test, log_likelihood_prob.T)
     return np.argmax(log_probs, axis=1)
 
-# Train the Multinomial Naive Bayes model
-prior_prob, likelihood_prob = calculate_probabilities_bernoulli(x_train, y_train)
-y_pred = predict_bernoulli(x_test, prior_prob, likelihood_prob)
+# Function to predict the labels of the test data according to bernoulli model
+def predict_bernoulli(x_features_test, prior_probabilities, likelihood_probabilites):
+    """In this prediction method, we are predicting the labels of the given features of the
+    instances of test data according to Bernoulli model by following the following formua: 
+        yi = argmax(logP(Y=yk) + log(multiplication tj*P(Xj/Y=yk)+(1-tj)*(1-P(Xj/Y=yk))) from j=1 to V)
+                y
+    Args:
+        x_features_train (numpy.2darray): number of features in each instance of the training data 
+        y_labels_train (numpy.1darray): labels of each instance of the training data
+        alpha (int): Dirichlet prior constant. Defaults to 1.
 
-# Calculate accuracy
+    Returns:
+        argmax(numpy.1darray): we create an array from the indices of the greatest values
+        over axis 1, which indicates which class each instance in the test set according to
+        our prediction
+    """
+    x_binary = (x_features_test > 0).astype(int)
+    log_likelihood_prob = np.where(likelihood_probabilites < 0.0000000001, -10**12, np.log(likelihood_probabilites, where = likelihood_probabilites > 0.0000000001))
+    log_likelihood_prob_inverse = np.where((1 - likelihood_probabilites) < 0.0000000001, -10**12, np.log((1 - likelihood_probabilites), where = (1 - likelihood_probabilites) > 0.0000000001))
+    log_probs = np.log(prior_probabilities) + np.dot(x_binary, log_likelihood_prob.T) + np.dot(1 - x_binary, log_likelihood_prob_inverse.T)
+    return np.argmax(log_probs, axis=1)
+
+##### MULTINOMIAL NAIVE BAYES WITH NO SMOOTHING #####
+
+# Train the Multinomial Naive Bayes with no smoothing
+prior_prob, likelihood_prob = calculate_probabilities_multinomial_nosmoothing(x_train, y_train)
+y_pred = predict_multinomial_bayes(x_test, prior_prob, likelihood_prob)
+
+# Calculate the accuracy of multinomial bayes with no smoothing
 accuracy = np.mean(y_pred == y_test)
-print(f'Accuracy: {accuracy:.3f}')
+print(f'Accuracy of Multinomial Bayes Model Without Smoothing: {accuracy:.3f}')
 
 # Create the Confusion Matrix for multinomial bayes with no smoothing
 conf_matrix = np.zeros((len(np.unique(y_test)), len(np.unique(y_test))))
 for index, label in enumerate(y_test):
     conf_matrix[y_pred[index], y_test[index]] += 1
 
-print('Confusion Matrix:')
+print('Confusion Matrix of Multinomial Bayes Model Without Smoothing:')
 print(conf_matrix)
+
+##### MULTINOMIAL NAIVE BAYES WITH SMOOTHING #####
+
+# Train the Multinomial Naive Bayes model with smoothing
+prior_prob, likelihood_prob = calculate_probabilities_multinomial_smoothing(x_train, y_train)
+y_pred = predict_multinomial_bayes(x_test, prior_prob, likelihood_prob)
+
+# Calculate the accuracy of multinomial bayes with smoothing
+accuracy = np.mean(y_pred == y_test)
+print(f'Accuracy of Multinomial Bayes Model With Smoothing: {accuracy:.3f}')
+
+# Create the Confusion Matrix for multinomial bayes with no smoothing
+conf_matrix = np.zeros((len(np.unique(y_test)), len(np.unique(y_test))))
+for index, label in enumerate(y_test):
+    conf_matrix[y_pred[index], y_test[index]] += 1
+
+print('Confusion Matrix of Multinomial Bayes Model With Smoothing:')
+print(conf_matrix)
+
+##### BERNOULLI MODEL #####
+
+# Train the Bernoulli Model
+prior_prob, likelihood_prob = calculate_probabilities_bernoulli(x_train, y_train)
+y_pred = predict_bernoulli(x_test, prior_prob, likelihood_prob)
+
+# Calculate the accuracy of the bernoulli model
+accuracy = np.mean(y_pred == y_test)
+print(f'Accuracy of Bernoulli Model: {accuracy:.3f}')
+
+# Create the Confusion Matrix for the bernoulli model
+conf_matrix = np.zeros((len(np.unique(y_test)), len(np.unique(y_test))))
+for index, label in enumerate(y_test):
+    conf_matrix[y_pred[index], y_test[index]] += 1
+
+print('Confusion Matrix of Bernoulli Model:')
+print(conf_matrix)
+
+finish_time = time.time()
+
+print("Time elapsed: ", finish_time - start_time, " seconds")
